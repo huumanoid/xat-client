98c00b5c826cae3298639e4447b0920b7a7c8743
huumanoid <huumanoid@hotmail.com>
2016-04-18 03:49:56 +0300

    periodic-reconnect mixin

---
+++ src/mixins/periodic-reconnect.js
@@ -0,0 +1,21 @@
+'use-strict';
+
+/**
+ * @param options
+ *  interval - interval between reconnects in milliseconds. Default is 2 hours.
+ *  only_connected - perform reconnect only if user is connected. Default is false.
+ *
+ */
+function periodic_reconnect(user, options) {
+    let interval = options.interval || 1000 * 60 * 60 * 2; //2 hours
+    let only_connected = options.only_connected || false;
+
+    setInterval(function () {
+        if (!only_connected || user.isConnected) {
+            user.end();
+            user.connect();
+        }
+    }, interval);
+}
+
+module.exports.bind = periodic_reconnect;
