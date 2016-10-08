97bae6fce1ab3b20537956ef2c1d9a0f1ca3b001
huumanoid <huumanoid@hotmail.com>
2016-10-08 03:24:19 -0400

    lurker options added

--- src/mixins/lurker-timeout.js
+++ src/mixins/lurker-timeout.js
@@ -3,11 +3,13 @@
 
 
 function lurkerTimeout(user, options) {
+    options = options || {};
+    var interval = options.interval || 1000 * 60 * 9;
     setInterval(function () {
         if (user.gotDone) {
             user.sendKeepAlive();
         }
-    }, 1000 * 60 * 9);
+    }, interval);
 }
 
 module.exports.bind = lurkerTimeout;
